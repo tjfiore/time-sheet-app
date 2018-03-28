3fb6b618b2cfc99d83cf711bbbcf1e25390d0f9d
tjfiore <tjaefiore@gmail.com>
2018-03-28 02:45:05 -0500

    imported my stylesheet

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
-import './App.css';
+import './stylesheets/style.css';
 import SignIn from "./components/auth/signin";
+import MainPage from "./components/mainpage";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Route exact path="/" component={SignIn} /> 
+        <Route exact path="/mainpage" component={MainPage} /> 
       </div>
     );
   }

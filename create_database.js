887180b8111b95f0d8bcb1e41a90e18a1067a5e5
TJae <tvickers@choicebusinesssolutions.net>
2018-03-28 12:48:51 -0500

    create db

--- create_database.js
+++ create_database.js
@@ -7,25 +7,38 @@ var con = mysql.createConnection({
 });
 
 con.connect(function (err) {
-  if (err){
-    console.log('Error connecting to db');
-  }else{
-    console.log("Connected!");
-  }
-  
+  if (err) throw err;
+  console.log("Connected!");
+ 
 
-  con.query("CREATE DATABASE timesheetdb", function (err, result) {
-    if (err) {
-      console.log('Error: ', err);
-    }else{
-      console.log("Database created");
-    }    
-  });
-
-  const sql = "CREATE TABLE employees (name VARCHAR(255), address VARCHAR(255))";
-  const sql2 = "CREATE TABLE clients (name VARCHAR(255), address VARCHAR(255))";
-  con.query([sql, sql2], function (err, result) {
+  con.query("CREATE DATABASE timesheet", function (err, result) {
     if (err) throw err;
-    console.log("Table created");
+      console.log("Database created");
   });
+    
+    
+  const employees = 'CREATE TABLE employees (id int(12) PRIMARY KEY AUTO_INCREMENT, email VARCHAR(255) UNIQUE NOT NULL,    password VARCHAR(255) NOT NULL,first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL)';
+  con.query(employees, function (err, result) {
+    if (err) throw err;
+    console.log("Employees Table created.");
+  });
+
+  const clients = 'CREATE TABLE clients (id INT AUTO_INCREMENT PRIMARY KEY, client_name VARCHAR(255) NOT NULL manager_name VARCHAR(255) NOT NULL, position VARCHAR(255) NOT NULL)';    
+  con.query(clients, function (err, result) {
+    if (err) throw err;
+    console.log("Clients Table created.");
+  });
+
+    // con.query(`CREATE PROCEDURE save_data (IN name varchar(255),IN address varchar(255),IN branch varchar(50))
+    //    BEGIN
+    //     insert into student (name,user_name,branch) values (in_name ,in_user_name,in_branch);
+    // END`, function (err, result) {
+    //   if (err) throw err;
+    //   console.log("Store procedure created.");
+    // });
+
+    
+
+
+  
 });

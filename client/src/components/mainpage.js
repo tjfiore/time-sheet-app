928493917183fee7aa73f846ed6055a649c9c936
TJae <tvickers@choicebusinesssolutions.net>
2018-03-28 12:50:00 -0500

    added functions

--- client/src/components/mainpage.js
+++ client/src/components/mainpage.js
@@ -9,7 +9,6 @@ class MainPage extends Component{
     first_name: '',
     last_name: '',
     address: '',
-    userAdd: '',
     title:'',
     s_time: '',
     e_time: '',
@@ -17,10 +16,20 @@ class MainPage extends Component{
     client: 'Select Client',
     clients: [],
     showfields: false,
+    showtimefield: false,
+    hourdiff: null,
+    mindiff: null,
     manager: '',
     position: '',
     date:'',
     dates: [],
+    titleError:'',
+    stimeError:'',
+    etimeError:'',
+    commentsError:'',
+    clientError: '',
+    dateError: '',
+    
    
   }
 
@@ -65,12 +74,7 @@ class MainPage extends Component{
     this.setState({dates: new_arr});
     
     
-  }
-
-
-
-
-   
+  }   
 
   handleSelectChange = (e) => {
     this.setState({client: e.target.value}) 
@@ -98,17 +102,68 @@ class MainPage extends Component{
   handleTimeChange = (e) => {
     this.setState({ e_time: e.target.value });
 
-    console.log('Start time: ',this.state.s_time);
-    console.log('End time: ', e.target.value);
+    const valuestart = this.state.s_time;
+    const valuestop = e.target.value;
+    
+    //create date format          
+    const hourStart = new Date("01/01/2007 " + valuestart).getHours();
+    const hourEnd = new Date("01/01/2007 " + valuestop).getHours();
+    const minStart = new Date("01/01/2007 " + valuestart).getMinutes();
+    const minEnd = new Date("01/01/2007 " + valuestop).getMinutes();
 
-  
+    let hourDiff = hourEnd - hourStart;    
+    let minDiff = minEnd - minStart;
+
+    this.setState({
+      hourdiff: (hourDiff < 0) ? hourDiff = hourStart - hourEnd : hourDiff,
+      mindiff: (minDiff < 0) ? minDiff = minStart - minEnd : minDiff,
+      showtimefield: true,
+    });
+    
     
   }
 
 
   handleSubmit = (e) => {
     e.preventDefault();
-    swal({ type: 'success', title: 'Successfully submitted!', text:""  }) 
+
+    if(!this.state.title){
+      this.setState({titleError: 'Reqiured'});
+    }else if(!this.state.client){
+      this.setState({clientError: 'Reqiured'});
+    }else if(!this.state.s_time){
+      this.setState({stimeError: 'Reqiured'});
+    }else if(!this.state.e_time){
+      this.setState({etimeError: 'Reqiured'});
+    }else if(!this.state.comments){
+      this.setState({commentsError: 'Reqiured'});
+    }else if(!this.state.date){
+      this.setState({dateError: 'Reqiured'});
+    }else{
+
+      const userdata ={
+        task_name: this.state.title,
+        // client: this.state.client,
+        start_time: this.state.s_time,
+        end_time: this.state.e_time,
+        comments: this.state.comments,
+        sdate: this.state.date,
+      }
+
+      postRequest('/submit', {userdata})
+      .then(res=>res.json())
+      .then(res => {
+        if(res){
+          console.log(res);
+          
+          swal({ type: 'success', title: 'Successfully submitted!', text:""  }) 
+        }
+      })
+      .catch(err => console.log(err));
+    }
+
+    
+    
     
   }
 
@@ -131,32 +186,41 @@ class MainPage extends Component{
         </div>
       </div>
     );
+    const rendertimeField = (       
+      <div className="form-group">       
+        <p>Time spent: {this.state.hourdiff} hours and {this.state.mindiff} minutes</p>
+      </div>      
+   
+    );
 
     return(
       <div className="align">
         <form onSubmit={this.handleSubmit} className="form-horizontal">
-
-          <div className="row">
-            <div className="col-sm-5">
-              <div className="form-group">
-                <label>User Name</label>
-                <input type="text" name="title" className="form-control" placeholder="Enter name" 
-                value={this.state.first_name +' '+ this.state.last_name} onChange={e => this.setState({ first_name: e.target.value })} />
+          { this.state.first_name && this.state.last_name && this.state.address ?
+            <div className="row">
+              <div className="col-sm-5">
+                <div className="form-group">
+                  <label>User Name</label>
+                  <input type="text" name="title" className="form-control" placeholder="Enter name" required 
+                  value={this.state.first_name +' '+ this.state.last_name} onChange={e => this.setState({ first_name: e.target.value })} />
+                </div>
               </div>
-            </div>
-            <div className="col-sm-7">
-              <div className="form-group">
-                <label>Address</label>
-                <input type="text" name="address" className="form-control" placeholder="Enter address" value={this.state.address} 
-                onChange={e => this.setState({ last_name: e.target.value })}  />
+              <div className="col-sm-7">
+                <div className="form-group">
+                  <label>Address</label>
+                  <input type="text" name="address" className="form-control" placeholder="Enter address" required 
+                  value={this.state.address} onChange={e => this.setState({ last_name: e.target.value })} />
+                </div>
               </div>
-            </div>
-          </div>
+            </div> :
+            <center><h2> NO User Name or Address Available</h2></center>
+          }
           
           <div className="form-group">
             <label>Task Title</label>
-            <input type="text" name="title" className="form-control" placeholder="Enter task name" 
-               onChange={e => this.setState({ title: e.target.value })} />            
+            <input type="text" name="title" className="form-control" placeholder="Enter task name" required
+               onChange={e => this.setState({ title: e.target.value })} />  
+             <span style={{color: 'red'}}>{this.state.titleError}</span>          
           </div>
 
           <div className="form-group">            
@@ -168,7 +232,8 @@ class MainPage extends Component{
                   {client.client_name}</option>
                 })
               }
-            </select>           
+            </select>  
+            <span style={{color: 'red'}}>{this.state.clientError}</span>         
           </div>
 
           {
@@ -184,30 +249,38 @@ class MainPage extends Component{
                 })
               }
             </select>
+            <span style={{color: 'red'}}>{this.state.dateError}</span>
           </div>
 
           <div className="row">
             <div className="col-sm-3">
             <div className="form-group">
                 <label>Start Time</label>
-              <input type="time" name="start_time" onChange={e => this.setState({ s_time: e.target.value })}  className="form-control" /> 
+              <input type="time" name="start_time" required onChange={e => this.setState({ s_time: e.target.value })}  className="form-control" /> 
+              <span style={{color: 'red'}}>{this.state.stimeError}</span>
             </div>       
             </div>
             <div className="col-sm-3">
             <div className="form-group">
               <label>End Time</label>
-              <input type="time" name="end_time" onChange={this.handleTimeChange} className="form-control" /> 
+              <input type="time" name="end_time" required onChange={this.handleTimeChange} className="form-control" />
+              <span style={{color: 'red'}}>{this.state.etimeError}</span> 
             </div>
             </div>
           </div>
 
+          {
+            this.state.showtimefield ? rendertimeField : null
+          }
+
           <div className="form-group">
             <label >Comments</label>
-            <textarea onChange={e => this.setState({ comments: e.target.value })} rows="5" className="form-control comment-area" /> 
+            <textarea required onChange={e => this.setState({ comments: e.target.value })} rows="5" className="form-control comment-area" /> 
+            <span style={{color: 'red'}}>{this.state.commentsError}</span>
           </div>
                    
       
-            <button type="submit" className="btn submitBtn">Submit</button>
+          <button type="submit" className="btn submitBtn">Submit</button>
        
         </form>
       </div>
